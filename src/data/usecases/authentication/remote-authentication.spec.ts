d98ec7a506558b569b2cd5c8abaf9dab0b5ca5ee
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-06-03 22:58:06 -0300

    refactor: move HttpPostClientSpy to its own file and folder

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,16 +1,8 @@
-import { HttpPostClient } from '../../protocols/http/http-post-client'
-import RemoteAuthentication from './remote-authentication'
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 describe('RemoteAuthentication', () => {
   it('shoud call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post(url: string): Promise<void> {
-        this.url = url
-        return await Promise.resolve()
-      }
-    }
-
     const url = 'api_url'
     const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,12 +1,12 @@
 import { HttpPostClient } from '../../protocols/http/http-post-client'
 
-export default class RemoteAuthentication {
-  constructor (
+export class RemoteAuthentication {
+  constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient
   ) {}
 
-  async auth (): Promise<void> {
+  async auth(): Promise<void> {
     await this.httpPostClient.post(this.url)
   }
 }
