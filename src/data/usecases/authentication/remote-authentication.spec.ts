bf13a5c11bce81f9092591d75b4b951c44da57fe
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-06-03 21:48:43 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,20 @@
+import { HttpPostClient } from '../../protocols/http/http-post-client'
+import RemoteAuthentication from './remote-authentication'
+
+describe('RemoteAuthentication', () => {
+  it('shoud call HttpPostClient with correct URL', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string
+      async post (url: string): Promise<void> {
+        this.url = url
+        return Promise.resolve()
+      }
+    }
+
+    const url = 'api_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
