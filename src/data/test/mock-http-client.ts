f8202b17434f002185bd74913b9d1a7e8abe7d73
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-06-04 13:33:14 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -5,8 +5,11 @@ import {
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: object
+
   async post(params: HttpPostParams): Promise<void> {
     this.url = params.url
+    this.body = params.body
     return await Promise.resolve()
   }
 }
---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,7 @@
+import { AuthenticationParams } from '../usecases/authentication'
+import faker from 'faker'
+
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
