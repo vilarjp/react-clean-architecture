fbbc1db3b7b39342f9c85ff72b01ac630a812386
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-06-04 22:04:48 -0300

    refactor: group imports in index.ts files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,9 @@
 import {
   HttpPostClient,
-  HttpPostParams
-} from '@/data/protocols/http/http-post-client'
-import {
+  HttpPostParams,
   HttpResponse,
   HttpStatusCode
-} from '@/data/protocols/http/http-response'
+} from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,5 +1,5 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '../models/account-model'
+import { AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '../models'
 import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
