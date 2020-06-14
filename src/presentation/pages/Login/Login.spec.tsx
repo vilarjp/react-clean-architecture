f20a301eead9599712c9bc12e1d326e622d9dac6
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-06-14 14:02:25 -0300

    chore: add react testing library

---
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Login from './Login'
+
+describe('should ', () => {
+  it('should ', () => {
+    render(<Login />)
+  })
+})
