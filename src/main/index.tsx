9a3b294854093ee172a6e71ed6ee7d6ac6c64ef1
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-06-14 13:36:34 -0300

    feat: add react router dom

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Login } from '@/presentation/pages'
+import { Router } from '@/presentation/router'
 
-ReactDOM.render(<Login />, document.getElementById('main'))
+ReactDOM.render(<Router />, document.getElementById('main'))
---
+++ src/presentation/router/router.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { Login } from '@/presentation/pages'
+
+import '@/presentation/styles/colors.scss'
+import '@/presentation/styles/global.scss'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
