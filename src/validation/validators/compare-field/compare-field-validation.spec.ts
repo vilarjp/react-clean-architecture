3aff03530aec1970d88310442a799e794ca38475
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-07-04 22:46:39 -0300

    refactor: change CompareFieldValidation implementation

--- src/validation/validators/compare-field/compare-field-validation.spec.ts
+++ src/validation/validators/compare-field/compare-field-validation.spec.ts
@@ -2,20 +2,21 @@ import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { CompareFieldValidation } from './compare-field-validation'
 
-const makeSut = (fieldToCompare: string): CompareFieldValidation =>
-  new CompareFieldValidation(faker.database.column(), fieldToCompare)
+const makeSut = (): CompareFieldValidation =>
+  new CompareFieldValidation(faker.database.column())
 
 describe('CompareFieldValidation', () => {
   it('should return error if match is invalid', () => {
-    const sut = makeSut(faker.random.word())
-    const error = sut.validate(faker.random.word())
+    const sut = makeSut()
+    const value = faker.random.word()
+    const error = sut.validate(value, faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
   it('should return falsy if compare is valid', () => {
+    const sut = makeSut()
     const value = faker.random.word()
-    const sut = makeSut(value)
-    const error = sut.validate(value)
+    const error = sut.validate(value, value)
     expect(error).toBeFalsy()
   })
 })
--- src/validation/validators/compare-field/compare-field-validation.ts
+++ src/validation/validators/compare-field/compare-field-validation.ts
@@ -2,12 +2,9 @@ import { FieldValidation } from '@/validation/protocols/field-validation'
 import { InvalidFieldError } from '@/validation/errors'
 
 export class CompareFieldValidation implements FieldValidation {
-  constructor(
-    readonly field: string,
-    private readonly valueToCompare: string
-  ) {}
+  constructor(readonly field: string) {}
 
-  validate(value: string): Error {
-    return value === this.valueToCompare ? null : new InvalidFieldError()
+  validate(value: string, secondValue: string): Error {
+    return value === secondValue ? null : new InvalidFieldError()
   }
 }
