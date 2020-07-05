39655910d15274b7fd03174431bee2106205e8cd
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-07-05 13:28:08 -0300

    test: ensure Login begin with correct initial statate

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -3,6 +3,10 @@ describe('Login', () => {
     cy.visit('/login')
   })
   it('should load page with correct initial state', () => {
-    cy.getByTestId('email-error').should('have.text', 'Campo obrigatório')
+    cy.getByTestId('email-error').should('contain.text', 'Campo obrigatório')
+    cy.getByTestId('password-error').should('contain.text', 'Campo obrigatório')
+    cy.getByTestId('button-wrap').should('have.attr', 'disabled')
+    cy.getByTestId('button-wrap').should('contain.text', 'Entrar')
+    cy.getByTestId('button-wrap').should('not.have.descendants')
   })
 })
