43002ccdab3de3d3d10bc373f2c0316d4bf860dc
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-07-05 22:01:39 -0300

    test: ensure SignUp present error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -15,4 +15,25 @@ describe('SignUp', () => {
 
     FormHelper.testButtonIsDisabled('button-wrap', true, 'Criar')
   })
+
+  it('should present error state if form is invalid', () => {
+    FormHelper.testFieldState('name', '', 'Campo obrigatório')
+    FormHelper.testFieldState(
+      'email',
+      faker.random.word(),
+      'Insira um e-mail válido'
+    )
+    FormHelper.testFieldState(
+      'password',
+      faker.random.alphaNumeric(4),
+      'O campo deve conter pelo menos 5 dígitos'
+    )
+    FormHelper.testFieldState(
+      'passwordConfirmation',
+      faker.random.alphaNumeric(4),
+      'Valor inválido'
+    )
+
+    FormHelper.testButtonIsDisabled('button-wrap', true, 'Criar')
+  })
 })
