20933c5a735575e4983bc59c224a7ca7a8b1d3f0
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-07-09 15:04:27 -0300

    chore: add webpack prod and dev configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,10 @@
-const path = require('path')
 const { DefinePlugin } = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
   module: {
     rules: [
       { test: /\.ts(x?)$/, loader: 'ts-loader', exclude: /node_modules/ },
@@ -30,20 +19,18 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: './public',
+    contentBase: './dist',
     writeToDisk: true,
     historyApiFallback: true,
     port: 3000
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
+    }),
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
     })
   ],
   devtool: 'inline-source-map'
-}
+})
