8af0a387b60a8d50c2bb15f06bf461d02e91baf6
Joao Paulo Vilar <vilarjp@hotmail.com>
2020-07-09 21:33:57 -0300

    feat: add LoadSurveyResult layout

---
+++ src/presentation/pages/SurveyResult/SurveyResult.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import FlipMove from 'react-flip-move'
+import { Header, Footer, Spinner } from '@/presentation/components'
+import Styles from './SurveyResult-styles.scss'
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResult}>
+      <Header />
+      <div className={Styles.content}>
+        <h2>Pergunta 1?</h2>
+        <FlipMove className={Styles.answersList}>
+          <li>
+            <img
+              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+              alt="survey"
+            />
+            <span className={Styles.answer}>Resposta</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li className={Styles.active}>
+            <img
+              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+              alt="survey"
+            />
+            <span className={Styles.answer}>Resposta</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li>
+            <img
+              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+              alt="survey"
+            />
+            <span className={Styles.answer}>Resposta</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button type="button">Voltar</button>
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Aguarde...</span>
+            <Spinner />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyResult
--- src/presentation/pages/index.ts
+++ src/presentation/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from './Login/Login'
 export { default as SignUp } from './SignUp/SignUp'
 export { default as SurveyList } from './SurveyList/SurveyList'
+export { default as SurveyResult } from './SurveyResult/SurveyResult'
